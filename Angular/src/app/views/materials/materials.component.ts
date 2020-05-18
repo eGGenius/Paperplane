c9cdc0fd2058b8ade9f6205be419d3736b16bfbc
pajaga <37742247+pajaga@users.noreply.github.com>
2020-05-18 23:56:34 +0200

    show materials + update material stock form

--- Angular/src/app/views/materials/materials.component.ts
+++ Angular/src/app/views/materials/materials.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/services/http.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-materials',
   templateUrl: './materials.component.html',
-  styleUrls: ['./materials.component.scss']
+  styleUrls: ['./materials.component.scss'],
 })
 export class MaterialsComponent implements OnInit {
 
-  constructor(private http: HttpService) { }
+  materialStock: any;
+
+  constructor(private http: HttpService) {}
 
   ngOnInit(): void {
+    this.http.getAllMaterialStock().subscribe((data) => {
+      this.materialStock = data;
+    });
+  }
 
+  onSubmit(data: any) {
+    // console.log('Amount: ' + data.amount);
+    this.http.updateMaterialStock(data.materialId, data.amount);
   }
 }
