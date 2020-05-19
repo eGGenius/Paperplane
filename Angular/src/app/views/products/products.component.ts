c142e6fb1dc5910e6b308314e12fadfe648c1d18
pajaga <37742247+pajaga@users.noreply.github.com>
2020-05-19 13:50:09 +0200

    show products and update product stock

--- Angular/src/app/views/products/products.component.ts
+++ Angular/src/app/views/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from 'src/app/services/http.service';
+import { Model } from 'src/assets/interfaces/Model';
 
 @Component({
   selector: 'app-products',
@@ -8,9 +9,34 @@ import { HttpService } from 'src/app/services/http.service';
 })
 export class ProductsComponent implements OnInit {
 
+  products: Model[];
+
   constructor(private http: HttpService) { }
 
   ngOnInit(): void {
+    this.http.getAllProductStock().subscribe((data: Model[]) => {
+      this.products = data;
+    });  
+  }
+  
+  onSubmit(data: any) {
+    // console.log('Amount: ' + data.amount);
+    this.http.updateProductStock(data.modelIdentifier, data.amount).subscribe(data => {
+      this.http.getAllProductStock().subscribe((data: Model[]) => {
+        this.products = data;
+      });
+    });
   }
 
 }
+
+
+//  updateProductStock(model: String, number: Number) {
+// }
+
+// export interface Model {
+//   "identifier": String;
+//   "materials": [{ "materialId": String, "number": Number }];
+//   "sellingPrice": Number;
+//   "stock": Number;
+// }
